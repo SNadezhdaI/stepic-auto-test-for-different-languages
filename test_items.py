5c17d3d3c281876a4f51b7cdf2169717b8a31c76
Nadezhda <79163793388@yandex.ru>
2021-10-19 20:44:57 +0300

    Rename function

--- test_items.py
+++ test_items.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_language(browser):
+def test_find_button_add_to_basket(browser):
     browser.get(link)
    
     input1 = browser.find_elements_by_class_name("btn-add-to-basket")
